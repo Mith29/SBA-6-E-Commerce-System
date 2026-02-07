6fef8afb5f0cd17d175f7d43cbd55ea22046d8f1
Mithra <mithra29.venu@gmail.com>
2026-02-06 22:12:03 -0500

    Added readme

--- E-Commerce-system/dist/main.js
+++ E-Commerce-system/dist/main.js
@@ -3,12 +3,10 @@ import { calculateDiscount } from "./utils/discountCalculator.js";
 import { fetchAPIData, products } from "./services/apiService.js";
 import { calculateTax } from "./utils/taxCalculator.js";
 import { NetworkError, DataError } from "./utils/errorHandler.js";
-
 const fetchButton = document.getElementById('fetchData');
 const productContainer = document.getElementById('productContainer');
 //Added event listener to fetch button to call main function on click
 fetchButton.addEventListener("click", main);
-
 // Main function to fetch data and display products
 async function main() {
     try {
@@ -27,55 +25,42 @@ async function main() {
         return;
     }
     // clear previous results
-       productContainer.textContent = ""; 
-
+    productContainer.textContent = "";
     // Display each product's details
-
     for (const product of products) {
         const item = new Product(product.id, product.title, product.category, product.price, product.discountPercentage);
-          const productDiv = document.createElement("div");
+        const productDiv = document.createElement("div");
         productDiv.classList.add("product");
-
         // Title
         const title = document.createElement("h3");
         title.textContent = `ID: ${item.id} ${item.title}`;
         productDiv.appendChild(title);
-
         // Category
         const category = document.createElement("p");
         category.textContent = `Category: ${item.category}`;
         productDiv.appendChild(category);
-
         // Price
         const price = document.createElement("p");
         price.textContent = `Price: $${item.price.toFixed(2)}`;
         productDiv.appendChild(price);
-
         // Discount
         const discount = document.createElement("p");
         discount.textContent = `Discount: ${item.discountPercentage}%`;
         productDiv.appendChild(discount);
-
         // Price after discount
         const priceAfterDiscount = document.createElement("p");
         priceAfterDiscount.textContent = `Price after discount: $${item.getPriceWithDiscount().toFixed(2)}`;
         productDiv.appendChild(priceAfterDiscount);
-
         // Discounted amount
         const discountedAmount = document.createElement("p");
         discountedAmount.textContent = `Discounted amount: $${calculateDiscount(item).toFixed(2)}`;
         productDiv.appendChild(discountedAmount);
-
         // Tax
         const tax = document.createElement("p");
         tax.textContent = `Tax: $${calculateTax(item).toFixed(2)}`;
         productDiv.appendChild(tax);
-
         // Add the product div to the container
         productContainer.appendChild(productDiv);
-
-
-
         // console.log(item.displayDetails());
         // console.log("Price after discount: " + item.getPriceWithDiscount().toFixed(2));
         // console.log("Discounted amount:" + calculateDiscount(item).toFixed(2));
@@ -83,5 +68,3 @@ async function main() {
         // console.log("----------------------------------");
     }
 }
-
-
--- E-Commerce-system/src/main.ts
+++ E-Commerce-system/src/main.ts
@@ -1,33 +1,86 @@
-import { Product} from "./models/product.js";
+import { Product } from "./models/product.js";
 import { calculateDiscount } from "./utils/discountCalculator.js";
 import { fetchAPIData, products } from "./services/apiService.js";
 import { calculateTax } from "./utils/taxCalculator.js";
 import { NetworkError, DataError } from "./utils/errorHandler.js";
 
-async function main(){
-   try{
-   await fetchAPIData();
-   }
-   
-     catch(error)  {
-        if (error instanceof NetworkError) {
-          console.log("Network Error", error.message);
-        } else if (error instanceof DataError) {
-          console.log("Data Error", error);
-        } else {
-          console.error("Unknown Error:", error);
-        }
-      }
-for (const product of products){
-   const item = new Product(product.id, product.title ,product.category, product.price, product.discountPercentage);
-   console.log(item.displayDetails());
-  
-   console.log("Price after discount: " + item.getPriceWithDiscount().toFixed(2));
-   console.log("Discounted amount:" + calculateDiscount(item).toFixed(2));
-   console.log("calculated tax for the product:" + calculateTax(item).toFixed(2));
-   console.log("----------------------------------");
-   
+const fetchButton = document.getElementById('fetchData')!;
+const productContainer = document.getElementById('productContainer')!;
+//Added event listener to fetch button to call main function on click
+fetchButton.addEventListener("click", main);
 
+// Main function to fetch data and display products
+async function main() {
+    try {
+        await fetchAPIData();
+    }
+    catch (error) {
+        if (error instanceof NetworkError) {
+            console.log("Network Error", error.message);
+        }
+        else if (error instanceof DataError) {
+            console.log("Data Error", error);
+        }
+        else {
+            console.error("Unknown Error:", error);
+        }
+        return;
+    }
+    // clear previous results
+       productContainer.textContent = ""; 
+
+    // Display each product's details
+
+    for (const product of products) {
+        const item = new Product(product.id, product.title, product.category, product.price, product.discountPercentage);
+          const productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+
+        // Title
+        const title = document.createElement("h3");
+        title.textContent = `ID: ${item.id} ${item.title}`;
+        productDiv.appendChild(title);
+
+        // Category
+        const category = document.createElement("p");
+        category.textContent = `Category: ${item.category}`;
+        productDiv.appendChild(category);
+
+        // Price
+        const price = document.createElement("p");
+        price.textContent = `Price: $${item.price.toFixed(2)}`;
+        productDiv.appendChild(price);
+
+        // Discount
+        const discount = document.createElement("p");
+        discount.textContent = `Discount: ${item.discountPercentage}%`;
+        productDiv.appendChild(discount);
+
+        // Price after discount
+        const priceAfterDiscount = document.createElement("p");
+        priceAfterDiscount.textContent = `Price after discount: $${item.getPriceWithDiscount().toFixed(2)}`;
+        productDiv.appendChild(priceAfterDiscount);
+
+        // Discounted amount
+        const discountedAmount = document.createElement("p");
+        discountedAmount.textContent = `Discounted amount: $${calculateDiscount(item).toFixed(2)}`;
+        productDiv.appendChild(discountedAmount);
+
+        // Tax
+        const tax = document.createElement("p");
+        tax.textContent = `Tax: $${calculateTax(item).toFixed(2)}`;
+        productDiv.appendChild(tax);
+
+        // Add the product div to the container
+        productContainer.appendChild(productDiv);
+
+
+
+        // console.log(item.displayDetails());
+        // console.log("Price after discount: " + item.getPriceWithDiscount().toFixed(2));
+        // console.log("Discounted amount:" + calculateDiscount(item).toFixed(2));
+        // console.log("calculated tax for the product:" + calculateTax(item).toFixed(2));
+        // console.log("----------------------------------");
+    }
 }
-}
-main();
+
